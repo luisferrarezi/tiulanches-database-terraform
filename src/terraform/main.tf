fc1b87451527a753531ab770ec37c97b6682f126
Luis <luisferrarezi@hotmail.com>
2024-05-02 19:14:22 -0300

    MongoDB

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -23,3 +23,40 @@ resource "azurerm_mysql_server" "tiulanches_mysql_srv" {
   ssl_enforcement_enabled           = true
   ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
+
+resource "azurerm_cosmosdb_account" "tiulanches_account" {
+  name                = "tiulanches-pagamentos"
+  location            = azurerm_resource_group.tiulanches_db_rg.location
+  resource_group_name = azurerm_resource_group.tiulanches_db_rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  capabilities {
+    name = "EnableMongoRoleBasedAccessControl"
+  }
+
+  consistency_policy {
+    consistency_level = "Strong"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.tiulanches_db_rg.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "tiulanches_mongodb" {
+  name                = "tlpagamento"
+  resource_group_name = azurerm_cosmosdb_account.tiulanches_account.resource_group_name
+  account_name        = azurerm_cosmosdb_account.tiulanches_account.name
+}
+
+resource "azurerm_cosmosdb_mongo_user_definition" "tiulanches_definition" {
+  cosmos_mongo_database_id = azurerm_cosmosdb_mongo_database.tiulanches_mongodb.id
+  username                 = var.db_username
+  password                 = var.db_password
+}
